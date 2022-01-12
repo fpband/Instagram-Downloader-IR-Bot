ae819367cfd14bd1293a203ce5cf56e3dd88ea1b
fpband <82159763+fpband@users.noreply.github.com>
2022-01-12 19:40:47 +0330

    Update main.py

--- Instaloader/main.py
+++ Instaloader/main.py
@@ -11,7 +11,7 @@ from .database.users_sql import get_info
 async def main(_, msg):
     if 'instagram.com' not in msg.text:
         return
-    status = await msg.reply('📂 کمی صبر کنید... | 📤 در حال آپلود...', quote=True)
+    status = await msg.reply('در حال آپلود ...', quote=True)
     pattern = re.compile(r'^(https?:[/][/])?(www\.)?instagram.com[/](p|reel)[/]([A-Za-z0-9-_]+)')
     try:
         matches = pattern.search(msg.text)
@@ -36,13 +36,13 @@ async def main(_, msg):
         photos, videos, caption = post_prep(path)
         if not photos and not videos:
             await status.delete()
-            await msg.reply("لینک ارسالی نامعتبر میباشد.")
+            await msg.reply("No Such Instagram Post Exists.")
             return
         if len(photos+videos) == 1:
             if caption:
-                caption += "\n\n🎀 آیدی ربات : @IR_InstagramDLBot"
+                caption += "\n\n✅ @IR_InstagramDlBot"
             else:
-                caption = "\n\n🎀 آیدی ربات : @IR_InstagramDlBot"
+                caption = "✅ @IR_InstagramDlBot"
             if photos:
                 for photo in photos:
                     await msg.reply_photo(photo, caption)
